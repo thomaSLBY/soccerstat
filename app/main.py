2dc44d11ae78b57fb401412a4b49d56b61cbd943
Thomas LEBLAY <thomasleblay98@gmail.com>
2021-08-14 09:53:29 +0200

    better, faster, stronger!

--- app/main.py
+++ app/main.py
@@ -6,11 +6,49 @@ from mongo.main import soccerstat_db
 
 
 appli = Flask(__name__)
+def prepare_data(data):
+    comps = []
+    seasons = []
+    matchweeks = []
+    games = []
+    i=0
+    for comp,info in data.items():
+        comps.append(comp)
+        seasons.append([])
+        matchweeks.append([])
+        games.append([])
+        j=0
+        for season in info[0]['seasons']:
+            seasons[i].append(season['name'])
+            matchweeks[i].append([])
+            games[i].append([])
+            k = 0
+            for week in season['matchweeks']:
+                matchweeks[i][j].append(week['week'])
+                games[i][j].append([])
+                l = 0
+                for game in week['games']:
+                    games[i][j][k].append([])
+                    games[i][j][k][l].append(game['home'])
+                    games[i][j][k][l].append(game['score'])
+                    games[i][j][k][l].append(game['away'])
+                    games[i][j][k][l].append(game['date'])
+                    games[i][j][k][l].append(game['link'])
+                    l += 1
+                k += 1
+            j += 1
+        i += 1
+    
+    return comps, seasons, matchweeks, games
 
 @appli.route('/')
 def home():
+    comps, seasons, matchweeks, games = prepare_data(soccerstat_db.get_data())
     return render_template('index.html',
-    data=soccerstat_db.get_data()
+    comps = comps,
+    seasons = seasons,
+    matchweeks = matchweeks,
+    games = games
     )
 
 @appli.route('/databases/')
